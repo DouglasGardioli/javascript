1fdf5a3448bcce62234721b5f624c30ccb938769
Douglas Gardioli <117394684+DouglasGardioli@users.noreply.github.com>
2023-01-24 10:28:29 -0300

    criei os ex008, 009, 010

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+
+console.log(`Dirija sempre usando cinto de segurança.`)
+
+if (vel > 60) {
+    console.log('MULTADO! Você ultrapassou a velocidade limite.')
+}
