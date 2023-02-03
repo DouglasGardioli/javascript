ca94a7572033517e637964580089586dbf097c4f
Douglas Gardioli <117394684+DouglasGardioli@users.noreply.github.com>
2023-02-03 18:08:59 -0300

    resolução ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,22 +1,32 @@
 function contar() {
-    let ini = document.getElementById('inicio')
-    let fim = document.getElementById('fim')
-    let pas = document.getElementById('passo')
-    let res = document.getElementById('resultado')
+    let ini = document.getElementById('icont')
+    let fim = document.getElementById('fcont')
+    let pas = document.getElementById('pcont')
+    res = document.getElementById('resultado')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.lenght == 0) {
-       
-        window.alert('[Erro] Faltam dados. Revise os números e tente novamente!')
-
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        alert('Erro! Há dados não preenchidos. Tente Novamente')
     } else {
-        r.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
-        let p = Number(pas.value)    
+        let p = Number(pas.value)
 
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} `
+        res.innerHTML = 'Contando: '
+
+        if (p <= 0) {
+            alert(`Passo [${p}] é inválido. Consideramos então Passo [1]`)
+            p = 1
         }
 
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} - `
+            }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} - `
+            }
+        }
+        res.innerHTML += 'Fim!'
     }
 }
