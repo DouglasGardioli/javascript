3e3af1ba91001e55a82bf37e30450088b4763577
Douglas Gardioli <117394684+DouglasGardioli@users.noreply.github.com>
2023-02-08 20:34:42 -0300

    parágrafos ao invés de quebra de linha

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,61 +1,62 @@
 
 var valores = []
 
-function addvalores() {
+function addvalores() { // Adicionando valores
 
     let num = document.getElementById('inum')
     let res = document.getElementById('resultado')
     let list = document.getElementById('valores-list')
     let n = Number(num.value)
 
-    if (valores.includes(n) || n < 1 || n > 100) {
+    if (valores.includes(n) || n < 1 || n > 100) { //Testando o número digitado
         alert('Valor inválido ou já atribuido!')
-    } else {
+    } else { //Senão estiver incorreto, o valor é adicionado
         valores[valores.length] = n
         let item = document.createElement('option')
         item.text = `Valor ${n} adicionado`
         list.appendChild(item)
+        res.innerHTML = ''
     }
-    res.innerHTML = ''
+    num.value = '' // Apagando valor da caixa e mantendo o foco
+    num.focus()
 }
 
-function finalizar() {
+function finalizar() { // finalizar a adição de valores e fazer todos os cálculos
 
-    if (valores.length < 1) {
+    if (valores.length < 1) { // Erro ao tentar finalizar sem valor
         alert('Digite os valores antes de finalizar!')
-    } else {
-        let item = document.createElement('p')
-
+    } else { // Tudo certo, hora de fazer os cálculos
         let res = document.getElementById('resultado')
         res.innerHTML = ''
-        res.innerHTML += `Ao todo, temos ${valores.length} números cadastrados <br>`
+        res.innerHTML += `<p>Ao todo, temos ${valores.length} números cadastrados</p>` //Total de numeros cadastrados
 
-        var maior = 0
+        var maior = valores[0] //Maior valor
 
         for (let c = 0; c < valores.length; c++) {
             if (valores[c] > maior) {
                 maior = valores[c]
             }
         }
-        res.innerHTML += `O maior valor digitado foi ${maior} <br>`
+        res.innerHTML += `<p>O maior valor digitado foi ${maior}</p>`
 
-        var menor = maior
+        var menor = valores[0] // Menor valor
+
         for (let c = 0; c < valores.length; c++) {
             if (valores[c] < menor) { 
                 menor = valores[c]
             }
         }
-        res.innerHTML += `O menor valor digitado foi ${menor}<br>`
+        res.innerHTML += `<p>O menor valor digitado foi ${menor}</p>`
 
-        var soma = 0
+        var soma = 0 // Soma dos valores
 
         for (let c = 0; c < valores.length; c++) {
             soma += valores[c]  
         }
-        res.innerHTML += `A soma dos valores é ${soma} <br>` 
+        res.innerHTML += `<p>A soma dos valores é ${soma} </p>` 
 
-        var media = soma / valores.length
+        var media = soma / valores.length // Média dos valores
 
-        res.innerHTML += `A média entre os valores é ${media} <br>`
+        res.innerHTML += `<p>A média entre os valores é ${media.toFixed(1)}</p>`
     }
 }
